869da31e2cbaf6370ef4447a065909ad6d8f500b
Just A YAM Farmer <mhe70@bloomberg.net>
2020-08-18 17:28:06 -0400

    remove price graph

--- src/static/js/yam_ycrv.js
+++ src/static/js/yam_ycrv.js
@@ -119,9 +119,6 @@ async function main() {
     _print_link(`Claim ${earnedYFFI} ${rewardTokenTicker}`, claim);
     _print_link(`Exit`, exit);
 
-  await _print24HourPrice("yam", rewardTokenTicker);
-
-
   hideLoading();
 
 
